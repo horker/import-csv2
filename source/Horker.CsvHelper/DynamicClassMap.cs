bdba778aff70d52ac3710dc8914920aca0abd043
horker <horker@users.noreply.github.com>
2019-09-20 20:50:38 +0900

    Consider PSObjects which creeps into -ColumnNameMap values.

--- source/Horker.CsvHelper/DynamicClassMap.cs
+++ source/Horker.CsvHelper/DynamicClassMap.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 using CsvHelper.Configuration;
@@ -26,13 +27,17 @@ namespace Horker.CsvHelper
 
             foreach (DictionaryEntry entry in map)
             {
-                var member = typeof(T).GetMember((string)entry.Value);
+                var member = typeof(T).GetMember(entry.Value.ToString());
                 if (member == null)
                     throw new ArgumentException($"Member '{entry.Value}' is not found in the class {typeof(T)}");
 
-                if (entry.Key is string name)
+                var key = entry.Key;
+                if (key is PSObject pso)
+                    key = pso.BaseObject;
+
+                if (key is string name)
                     Map(typeof(T), member[0]).Name(name);
-                else if (entry.Key is int index)
+                else if (key is int index)
                     Map(typeof(T), member[0]).Index(index);
                 else
                     throw new ArgumentException("Key of -ColumnNameMap should be a string or int");
